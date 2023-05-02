84864401b76b3e984452eb2648b5add04febdbae
RohanM <rohan13maharjan@gmail.com>
2023-05-02 14:13:26 +0545

    Band sampling successfully done !

---
+++ Assets/Scripts/BandVisualizers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandVisualizers : MonoBehaviour
+{
+    public float _maxScale,_scaleMultiplier;
+    public int _band;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, Audio._requiredBands[_band] * _maxScale * _scaleMultiplier, transform.localScale.z);
+    }
+}
